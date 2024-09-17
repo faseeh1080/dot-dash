926ff101305c9881213599b1a7d3179e9c597580
FaseehWeb <faseeh1080@outlook.com>
2024-09-17 21:17:14 +0530

    Separated UI and Viewport into two surfaces.

--- classes.py
+++ classes.py
@@ -8,10 +8,12 @@ class Dot:
         self.position = (random.randint(1, 1267), random.randint(1, 707))
         self.circlewidth = 26
         self.color = (255, 0, 0)
-        self.start_time = time.time()
+
+        self.start_time = time.time() # To calculate response time.
         self.response_time = 0
-        self.response_time_list = []
+        self.response_time_list = [] # To store response times to calculate the average.
     
+    # Refresh the mouse position when clicked. Returns the response time.
     def refresh(self, screen,mse_buttons, mse_buttons_previous_frame, mse_pos):
         if mse_buttons[0] and not mse_buttons_previous_frame[0]:
             x = self.position[0] - mse_pos[0]
@@ -27,8 +29,18 @@ class Dot:
         pygame.draw.circle(screen, self.color, self.position, self.circlewidth)
         return self.response_time
 
+    # Returns the average response time from response_time_list. !--- Can return None value --!
     def calculate_avg_response_time(self):
         if self.response_time_list:
             average_response_time = sum(self.response_time_list) / len(self.response_time_list)
-        else: average_response_time = 0
+        else: average_response_time = None
+
         return average_response_time
+    
+class ResponseTimeLabel:
+    def __init__(self):
+        pass
+    def refresh(self, ui, response_time):
+        font = pygame.font.SysFont(None, 24)
+        text_surface = font.render("Response time: " + str(response_time) + " seconds", True, (255, 255, 255))
+        ui.blit(text_surface, (6, 6))
--- main.py
+++ main.py
@@ -3,15 +3,20 @@ from classes import *
 
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
+viewport = pygame.Surface((1280, 720))
+ui = pygame.Surface((1280, 720), pygame.SRCALPHA)
 clock = pygame.time.Clock()
 running = True
 
+mse_buttons = (False, False, False)
 mse_buttons_previous_frame = (False, False, False)
 response_time = 0
 
-font = pygame.font.SysFont(None, 24)
-score = font.render("Response time: " + str(response_time), True, (255, 255, 255))
+# Viewport Classes:
 circle1 = Dot()
+
+# UI classes:
+response_time_label = ResponseTimeLabel()
 
 while running:
     for event in pygame.event.get():
@@ -20,14 +25,19 @@ while running:
     mse_buttons = pygame.mouse.get_pressed()
     mse_pos = pygame.mouse.get_pos()
 
+
     screen.fill("black")
 
-    response_time = circle1.refresh(screen,mse_buttons, mse_buttons_previous_frame, mse_pos)
-    shrinked_response_time = str(response_time)[:4]
-    score = font.render("Response time: " + shrinked_response_time + " seconds", True, (255, 255, 255))
-    screen.blit(score, (4, 4))
+    # RENDERING THE VIEWPORT:
+    viewport.fill((0, 0, 0))
+    response_time = circle1.refresh(viewport, mse_buttons, mse_buttons_previous_frame, mse_pos)
 
+    # RENDERING THE UI:
+    ui.fill((0, 0, 0, 0))
+    response_time_label.refresh(ui, response_time)
 
+    screen.blit(viewport, (0, 0))
+    screen.blit(ui, (0, 0))
     pygame.display.flip()
 
     clock.tick(60)
